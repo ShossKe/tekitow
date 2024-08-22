fd38c6b8ed74a545f7953a6973ff3f2178d63aa4
ShossKe <shosuke.uenishi@outlook.jp>
2024-08-22 12:47:53 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,4 @@
-from tekitow import prime.isprime as isprime
-from tekitow import prime.isprime_num as isprime_num
-from tekitow import prime.primeslist as primeslist
+from . import prime
 
-from tekitow 
+from . import oasobi2
 
